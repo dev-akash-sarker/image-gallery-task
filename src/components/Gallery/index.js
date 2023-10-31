f0e5bca59cefa9cb02b237ed41264bf47b995489
dev-akash-sarker <akashsarker210@gmail.com>
2023-11-01 03:06:34 +0600

    second commit

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./style.css";
+export default function Gallery() {
+  return (
+    <>
+      <div className="galleryWrapper">
+        <div className="gallery_header">
+          <h4>Gallery</h4>
+        </div>
+        <hr />
+        <div className="grid">
+          <div className="item">1</div>
+          <div className="item">2</div>
+          <div className="item">3</div>
+          <div className="item">4</div>
+          <div className="item">5</div>
+          <div className="item">6</div>
+          <div className="item">7</div>
+          <div className="item">8</div>
+          <div className="item">9</div>
+          <div className="item">10</div>
+          <div className="item">11</div>
+          <div className="item">12</div>
+        </div>
+      </div>
+    </>
+  );
+}
