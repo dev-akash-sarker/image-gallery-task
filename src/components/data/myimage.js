742e84c42d98fbd47d378b95ac2c96104cafebf6
dev-akash-sarker <akashsarker210@gmail.com>
2023-11-02 01:22:47 +0600

    fixing

---
+++ src/components/data/myimage.js
@@ -0,0 +1,33 @@
+export const myData = [
+  { link: "./assets/images/gallery/image-11.jpeg" },
+  {
+    link: "./assets/images/gallery/image-1.webp",
+  },
+  {
+    link: "./assets/images/gallery/image-2.webp",
+  },
+  {
+    link: "./assets/images/gallery/image-3.webp",
+  },
+  {
+    link: "./assets/images/gallery/image-5.webp",
+  },
+  {
+    link: "./assets/images/gallery/image-6.webp",
+  },
+  {
+    link: "./assets/images/gallery/image-7.webp",
+  },
+  {
+    link: "./assets/images/gallery/image-8.webp",
+  },
+  {
+    link: "./assets/images/gallery/image-9.webp",
+  },
+  {
+    link: "./assets/images/gallery/image-4.webp",
+  },
+  {
+    link: "./assets/images/gallery/image-10.jpeg",
+  },
+];
